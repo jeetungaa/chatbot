ff8ff485a99971cc653f9f8ba1170694485a1b10
jeetungaa <vaasumarothia45@gmail.com>
2024-07-10 17:46:58 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect('mongodb://127.0.0.1:27017/new');
 
 // Login endpoint
 app.post("/login", (req, res) => {
