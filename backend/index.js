855e4733daa72ec5a37bf4d787775f6466d70d7f
jeetungaa <vaasumarothia45@gmail.com>
2024-07-12 16:58:49 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -73,8 +73,8 @@ app.post("/chatbot", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
